2f4e05fd07ef201a1dd06a31b2d28cb3a0d0d710
Callum <callumk0104@gmail.com>
2022-03-16 18:02:31 +0000

    Added placeholder methods for CRUDPanel Manager

--- DVSCDesktop/src/guimanagers/CRUDPanelManager.java
+++ DVSCDesktop/src/guimanagers/CRUDPanelManager.java
@@ -1,6 +1,8 @@
 package guimanagers;
 
 import gui.CRUDPanel;
+import domain.Garage;
+import java.util.Date;
 
 public class CRUDPanelManager {
 	
@@ -9,6 +11,54 @@ public class CRUDPanelManager {
 	public CRUDPanelManager() {
 		
 		CRUDPanel = new CRUDPanel();
-			
+
+	}
+
+	public void populateGarageList(String[] garageList){
+
+	}
+
+	public void addGarage(Garage garage){
+
+	}
+
+	public void removeGarage(int index){
+
+	}
+
+	public void changeGarageNameValue(String garageName){
+
+	}
+
+	public void changeGarageEmailValue(String emailAddress){
+
+	}
+
+	public void changeGaragePhoneNumber(String phoneNumber){
+
+	}
+
+	public void changeYearPaid(Boolean yearPaid){
+
+	}
+
+	public void changeCalibrationDate(Date calibrationDate){
+
+	}
+
+	public void populateInstrumentList(String[] instrumentList){
+
+	}
+
+	public void addInstrument(Instrument instrument){
+
+	}
+
+	public void removeInstrument(int index){
+
+	}
+
+	public void saveChanges(){
+
 	}
 }
