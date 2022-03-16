156c2ca437b373d093dfd4c8d0b4a22388aa5f99
Callum <callumk0104@gmail.com>
2022-03-16 16:53:02 +0000

    Updated Garage Class
    - Added private variables for relevant garage information
    - Made constructor method for Garage
    - Added getters for all private variables

--- DVSCDesktop/src/domain/Garage.java
+++ DVSCDesktop/src/domain/Garage.java
@@ -1,5 +1,50 @@
 package domain;
 
-public class Garage {
+import java.util.Date;
 
+/**
+ * @author Callum
+ */
+
+public class Garage {
+	
+	private String vts;
+	private String garageName;
+	private String ownerName;
+	private String emailAddress;
+	private String telephoneNum;
+	private Date paidUntil;
+	
+	public Garage(String vts, String garageName,String ownerName, String emailAddress, String telephoneNum, Date paidUntil){
+		this.vts = vts;
+		this.garageName = garageName;
+		this.ownerName = ownerName; 
+		this.emailAddress = emailAddress;
+		this.telephoneNum = telephoneNum;
+		this.paidUntil = paidUntil;
+	}
+
+	public String getVts() {
+		return vts;
+	}
+
+	public String getGarageName() {
+		return garageName;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTelephoneNum() {
+		return telephoneNum;
+	}
+
+	public Date getPaidUntil() {
+		return paidUntil;
+	}
 }
