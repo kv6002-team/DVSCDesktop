082bcc659b66ebbc8706f9c209d77aec049353c6
Callum <callumk0104@gmail.com>
2022-03-16 16:13:57 +0000

    Set up CRUDPanelManager class
    CRUDPanelManager structure set up ready to implement methods.

--- DVSCDesktop/src/guimanagers/CRUDPanelManager.java
+++ DVSCDesktop/src/guimanagers/CRUDPanelManager.java
@@ -1,6 +1,14 @@
 package guimanagers;
 
+import gui.CRUDPanel;
+
 public class CRUDPanelManager {
 	
+	private CRUDPanel CRUDPanel;
 	
+	public CRUDPanelManager() {
+		
+		CRUDPanel = new CRUDPanel();
+			
+	}
 }
