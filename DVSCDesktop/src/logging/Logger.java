c59d42bb56363d53b91428451367f9f70301a76d
Scrub <scott-donaldson@outlook.com>
2022-03-26 14:23:36 +0000

    Added enums to logger

--- DVSCDesktop/src/logging/Logger.java
+++ DVSCDesktop/src/logging/Logger.java
@@ -1,5 +1,24 @@
 package logging;
 
+import connection.Connection;
+
 public class Logger {
-	
+	private Connection con = new Connection();
+	public static enum TYPE {
+		LOGIN_EVENT,
+		MESSAGE_EVENT,
+		DATA_CREATED_EVENT,
+		DATA_UPDATED_EVENT,
+		DATA_DELETED_EVENT,
+		CONNECTION_EVENT
+	}
+	public static enum LEVEL {
+		INFO,
+		ALERT,
+		WARN,
+		CRITICAL
+	}
+	public void log(TYPE logType,LEVEL logLevel, String message) {
+		
+	}
 }
